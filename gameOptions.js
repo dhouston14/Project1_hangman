defb99351d7398aadb16ea7ab10dc50c2492a9de
Dominick Houston <dominick.houston14@gmail.com>
2017-02-01 09:56:48 -0500

    project complete

--- gameOptions.js
+++ gameOptions.js
@@ -1,13 +1,86 @@
-var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-       'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-       't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+// Javascript (Dom's Hangman)
 
-var answers = [
-["T", "R", "E", "E", "H", "O", "U", "S", "E"],
-  ["J","A","V","A","S","C","R","I","P","T"],
-  ["W","E","B","D","E","S","I","G","N"],
-  ["E","D","U","C","A","T","I","O","N"],
-  ["C","H","O","C","O","L","A","T","E"],
-  ["G","E","R","M","A","N","Y"]
-]
+var playGame = true;
+var words = new Array("CHELSEA", "BULLDOG", "PULPFICTION", "ELEPHANT", "JORDANS");
+var letters_guessed = "";
+var answer = "";
+var letters_clicked = "";
+var wrongAnswers = 0;
+
+function game(d) {
+  if (playGame == false){
+    return;
+}
+
+  if (letters_clicked.indexOf(d) != -1) {
+    return;
+}
+
+letters_clicked += d;
+document.game.usedLetters.value = letters_clicked;
+
+if (letters_guessed.indexOf(d) != -1) {
+ // correct letter guess
+position = 0;
+temp_mask = answer;
+
+while (letters_guessed.indexOf(d, position) != -1) {
+  position = letters_guessed.indexOf(d, position);
+  end = position + 1;
+
+  start_text = temp_mask.substring(0, position);
+  end_text = temp_mask.substring(end, temp_mask.length);
+
+  temp_mask = start_text + d + end_text;
+  position = end;
+  }
+
+  answer = temp_mask;
+  document.game.displayWord.value = answer;
+
+    if (answer.indexOf("#") == -1) {
+      alert("Congrats! You have Won. Drinks on the house!");
+      console.log("Game Complete...");
+      playGame = false;
+    }
+    }else{
+    // incorrect letter guess
+      wrongAnswers += 1;
+
+    if (wrongAnswers == 10) {
+      // GAME OVER
+      alert("Oh buddy you lost, here's a ribbon on accomplishment");
+      console.log("GAME OVER...");
+      playGame = false;
+    }
+  }
+}
+
+function selectWord() {
+  playGame = true;
+  random_number = Math.round(Math.random() * (words.length - 1));
+  letters_guessed = words[random_number];
+  // display masked word
+  masked_word = hideAnswer(letters_guessed);
+  document.game.displayWord.value = masked_word;
+  answer = masked_word;
+}
+
+function hideAnswer(m) {
+  hide = "";
+  word_length = m.length;
+
+  for (i = 0; i < word_length; i ++) {
+    hide += "#";
+  }
+    return hide;
+}
+
+function reset() {
+  selectWord();
+  document.game.usedLetters.value = "";
+  letters_clicked = "";
+  wrongAnswers = 0;
+
+}
